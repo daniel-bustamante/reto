c83d2c2247d388bff7fc82657793d5dc5b6b7a31
Daniel Bustamante <danbuva@hotmail.com>
2016-08-13 13:58:55 -0500

    Mini Reto Coursera

--- Fuentes/MiniReto.playground/Contents.swift
+++ Fuentes/MiniReto.playground/Contents.swift
@@ -1,5 +1,40 @@
-//: Playground - noun: a place where people can play
+/* Mini reto:
+ 
+ Programa un playground que evalúe un rango de números en base a 4 reglas de impresión.
+ 
+ Generar un rango de 0 a 100, incluye el número 100 en el rango.
+ Tienes que iterar el rango completo, utilizando la sentencia for para obtener cada número del rango y aplicar las siguientes reglas de impresión.
+ Al evaluar cada número debes aplicar las siguientes reglas:
+ - Si el número es divisible entre 5, imprime: # el número  + “Bingo!!!”
+ 
+ - Si el número es par, imprime: # el número + “par!!!”
+ 
+ - Si el número es impar, imprime: # el número + “impar!!!”
+ 
+ - Si el número se encuentra dentro de un rango del 30 al 40, imprime: # el número +  “Viva Swift!!!”
+ 
+ Debes de usar la interpolación de variables para realizar la impresión de cada número.
+ 
+ */
 
 import UIKit
 
-var str = "Hola"
+let serie = 0...100
+
+for num in serie {
+    
+    if num % 5 == 0 && num != 0 {
+        print("\(num) Bingo!!!")
+    }
+    if num % 2 == 0 {
+        print("\(num) Par!!!")
+    } else {
+        print("\(num) Impar!!!")
+    }
+    
+    if num >= 30 && num <= 40 {
+        print("\(num) Viva Swift!!!")
+    }
+}
+
+// Daniel Bustamante.
